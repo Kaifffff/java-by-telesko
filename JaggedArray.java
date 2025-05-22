98d90f47ec18c8e38901d48eff78e90b9ee39580
kaif <shaikhkaif3258@gmail.com>
2025-05-22 17:10:14 +0530

    jagged array

--- JaggedArray.java
+++ JaggedArray.java
@@ -1,5 +1,22 @@
 public class JaggedArray {
     public static void main(String []args){
-        
+        int num[][] = new int[3][];
+        num[0] = new int[4];
+        num[1] = new int[3];
+        num[2] = new int[2];
+
+        for(int i = 0 ; i < num.length ; i++){
+            for(int j = 0 ; j < num[i].length ; j++){
+                num[i][j] = (int)(Math.random() * 10);
+            } 
+        }
+
+        for(int i = 0;i < num.length ; i++){
+            for(int j = 0 ; j < num[i].length ; j++){
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
