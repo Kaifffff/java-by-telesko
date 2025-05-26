6977202ccc1519b92553ed4c705f93a55d71ae84
kaif <shaikhkaif3258@gmail.com>
2025-05-26 22:20:39 +0530

    String in java

---
+++ StringT.java
@@ -0,0 +1,9 @@
+class StringT{
+    public static void main(String[] args) {
+        // String name = new String("Kaif"); String is a class java automatic created ofject of class we do not need to create object of class
+        String name = "kaif";
+        System.out.println(name.concat(" shaikh"));
+        System.out.println(name.charAt(0));
+
+    }
+}
